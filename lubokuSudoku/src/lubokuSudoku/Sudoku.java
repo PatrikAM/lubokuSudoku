2af8975f4c6c8106b8c5d91571eba1db9769a1e8
karban <karban@UCEBNA-PC-M-06.gjkt.local>
2017-12-21 14:44:43 +0100

    ne

--- lubokuSudoku/src/lubokuSudoku/Sudoku.java
+++ lubokuSudoku/src/lubokuSudoku/Sudoku.java
@@ -1,20 +1,11 @@
 package lubokuSudoku;
 
 public class Sudoku {
-	private final int[] sudoku = new int[91];
 	
-	public int[] getRadek(int r){
-		//r je od 0 do 8
+	private static int[] sudoku = new int[81];
+	
+	public int[] getRadek(){
 		return null;
 	}
-	
-	public int[] getSloupec(int s){
-		//s je od 0 do 8
-		return null;
-	}
-	
-	public int[] getCtverec(int c){
-		// int c musí bıt jedno z èísel: 0 3 6 27 30 33 54 57 60, aby šlo o ètverec ve kterém se èísla nesmí opakovat!
-		return null;
-	}
+
 }
