c2f9787d19349c6a2c054781f414fe507a217b4c
michl <michl@PM>
2017-12-21 14:16:23 +0100

    Hi

---
+++ lubokuSudoku/src/lubokuSudoku/CheckController.java
@@ -0,0 +1,14 @@
+package lubokuSudoku;
+
+public class CheckController {
+
+	final int x = Sudoku().get.Pole();
+
+	public static boolean check() {
+		boolean x;
+		if (x = null || x == Vzor().getPole()) {
+			return x;
+		}
+	}
+
+}
