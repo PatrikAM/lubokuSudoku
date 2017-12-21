6577c5b1f64576e90dfe32b8748b4c0d4545e41a
karban <karban@UCEBNA-PC-M-06.gjkt.local>
2017-12-21 14:54:39 +0100

    možná

--- lubokuSudoku/src/lubokuSudoku/Sudoku.java
+++ lubokuSudoku/src/lubokuSudoku/Sudoku.java
@@ -3,9 +3,24 @@ package lubokuSudoku;
 public class Sudoku {
 	
 	private static int[] sudoku = new int[81];
+	private int r; //øádek 1 a 9
+	private int s; //sloupec 1 a 9
+	private int c; //ètvcerec 1 a 9 - ètverce postupují po øádcích, indexy 1 a 3 oznaèují
 	
-	public int[] getRadek(){
+	public int[] getRadek(int r){
+		int[] radek = new int[9];
 		return null;
 	}
+	
+	public int[] getSloupec(int s){
+		int[] sloupec = new int[9];
+		return null;
+	}
+	
+	public int[] getCtverec(int c){
+		int[] ctverec = new int[9];
+		return null;
+	}
+	
 
 }
